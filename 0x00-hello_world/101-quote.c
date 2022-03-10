b56ea669862ccf8a5f75da682c7a04d2eee53c2b
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-10 10:52:41 -0800

    understanding UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@ ssize_t pwrite(int fildes, const void *buf, size_t nbyte, off_t offset);
  *
  * Return: Always 1 (Success)
  */
-int main(void)
+char main(void)
 {
 	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
