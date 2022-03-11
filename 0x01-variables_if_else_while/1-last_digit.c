bc23b2bd34d84672b72a30fc27b349e0c87f95c0
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-11 08:56:09 -0800

    Printing last digits of random numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main (void)
 	} else if (m == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, m);
-	} else (6 < m > 0)
+	} else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
