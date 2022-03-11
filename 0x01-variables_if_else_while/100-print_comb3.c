1c0066c66122a6aa2f5bbff4aa600e08a3740779
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-11 15:00:54 -0800

    Printing number combination of double digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = '0';
+
+	for (int ch = '0'; ch < '100'; ch++)
+	{
+		putchar((ch/10)+'0');
+		putchar((ch%10)+'0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\0');
+	return (0);
+}
