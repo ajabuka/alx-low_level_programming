6164fe9a787c56528ee10ade7668f33517b77807
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-13 05:48:11 -0700

    ALX is playing on my intelligence

---
+++ 0x01-variables_if_else_while/102-print_com5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = '0';
+	int d = '0';
+	int e = '0';
+	int f = '0';
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (d = '0'; d <= '9'; d++)
+		{
+			for (e = '0'; e <= '9'; e++)
+			{
+				for (f = '0'; f <= '9'; f++)
+				{
+
+					if ((c != d && c < d) && (e != f && e < f))
+					{
+						putchar(c);
+						putchar(d);
+						putchar(' ');
+						putchar(e);
+						putchar(f);
+						if ((c == '9' && d == '9') && (e == '9' && f == '9'))
+						{
+							break;
+						}
+						if (c < '7')
+						putchar(',');
+						if (c < '7')
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
