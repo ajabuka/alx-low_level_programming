aeb465fbee53ed3967efd713217d42149284492e
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-15 15:10:06 -0700

    Write a function that prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a value
+ * @c: Standard input
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int c)
+{
+	c = c % 10
+	{
+		return (c);
+	}
+	_putchar('\n');
+}
