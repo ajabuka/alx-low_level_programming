6887eed79f6dd146e8769c438b4ea296990633f3
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-15 07:55:54 -0700

    Write a function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - Checks if an alphabet is in lower case
+ *
+ * Return: Always 0 (Success)
+ * */
+
+int _islower()
+{
+	int r;
+
+	if r == _islower
+	{
+		_putchar('1')
+	}
+	else
+	{
+		_putchar('0')
+	}
+	_putchar('\n')
+}
