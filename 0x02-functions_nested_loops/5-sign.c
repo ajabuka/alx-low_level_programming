2b724428a4c880fa4ec704d2df1d5b658f87b2b3
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-15 08:46:07 -0700

    Write a function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks if a number is positive or negative
+ * @n: stardard input
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	if else (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar(-);
+	}
+	_putchar('\n');
+}
