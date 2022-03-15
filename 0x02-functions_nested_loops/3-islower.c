623d51f12e29b78f532bc593fa3499132b7526f5
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-15 08:02:44 -0700

    Write a function that checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 
 /**
  * _islower - Checks if an alphabet is in lower case
- *
+ * @c: stardard input
  * Return: Always 0 (Success)
  * */
 
@@ -11,13 +11,13 @@ int _islower(int c)
 {
 	int r;
 
-	if (r == _islower)
+	if (r = _islower)
 	{
-		_putchar('1')
+		_putchar('1');
 	}
 	else
 	{
-		_putchar('0')
+		_putchar('0');
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
