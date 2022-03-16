d7b41af7c9b6b18c1e794ca1e81170be6915ed2b
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-16 02:53:25 -0700

    Print sum of multiples of 3 and 5 in numbers below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,16 +2,16 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int n, sum=0;
+	int n, sum = 0;
 	for (n = 0; n < 1024; n++)
 	{
-		if ((n % 3 == 0) || (n % 5 == 0) || (n % 6 == 0))
+		if ((n % 3 == 0) || (n % 5 == 0))
 		{
 			sum = sum + n;
 		}
