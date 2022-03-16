ed19647cee7f5d8dbd51ef1de6e6c3c6c0ac160b
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-16 12:54:07 -0700

    Debugging faulty loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -11,6 +12,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
 	/*putchar(i);*/
