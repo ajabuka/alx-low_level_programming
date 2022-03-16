cb12e2a2d7397c232767fb0930c37ccf1a6776e4
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-16 03:56:58 -0700

    Write a program that prints the even Fibonacci up to 4,000,000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Printing Fibonacci series, this adds previous two numbers as next
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int Fn;
+	long int t1 = 1;
+	long int t2 = 2;
+	long int nt;
+
+	printf("%ld", t2);
+	for (Fn = 3; Fn <= 50; ++Fn)
+	{
+		nt = t1 + t2;
+		t1 = t2;
+		t2 = nt;
+	}
+	if (nt < 4000000);
+	{
+		nt != (nt % 10 == 1);
+		printf(", %ld", nt);
+	}
+	printf("\n");
+	return (0);
+}
