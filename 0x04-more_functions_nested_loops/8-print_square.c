9182804cf5f746d03dc0f0989f7929fcdabbbd53
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-17 07:35:40 -0700

    Write a function that prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int size = '0';
+	int d = '0';
+
+	for (d = '0'; d <= '9'; d++)
+	{
+
+		for (size = '0'; size <= '9'; a++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
