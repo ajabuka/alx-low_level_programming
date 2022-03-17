4f2d90595f5b9cebff720e257b00f76237a35981
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-17 14:15:40 -0700

    Write a program that finds and prints the largest prime factor of the number 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - finds all prime factors of a given number
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+	long int a;
+	long int n = 612852475143;
+
+	for (a = 2; a <= n; a++)
+	{
+		if (n % a == 0)
+		{
+			n /= a;
+			a--;
+		}
+	}
+	printf("%ld\n", a);
+	return (0);
+}
