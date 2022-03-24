184f255feb59dd643857612ef7c79a8d3a1725b0
Jesubukade Ajesubukade Ajakaye <ajakaye.bukaajakaye.bukade@gmail.com>
2022-03-24 06:43:35 -0700

    Using pointer to redirect into an array

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - prints a number in an array
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
@@ -9,7 +14,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write tour line of code on line 19
+	 * write tour line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
