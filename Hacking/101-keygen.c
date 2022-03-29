12e46306fe85404c61c47ed5a70569a99aee4dc2
Jesubukade Ajakaye <ajakaye.bukade@gmail.com>
2022-03-29 19:25:44 +0100

    Hackable files

---
+++ Hacking/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - randomly generates password
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int r =0;
+	int i = 0;
+
+	srand((unsigned int) time(NULL));
+	while (i < 2772)
+	{
+		r = rand() % 128;
+		if ((i + r) > 2772)
+			break;
+		i = i + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - i));
+	return (0);
+}
