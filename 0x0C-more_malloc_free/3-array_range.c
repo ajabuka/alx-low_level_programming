d63418037d9814ca23f004cd9d23b5658bd46459
Jesubukade Ajakaye <ajakaye.bukade@gmail.com>
2022-04-08 01:03:00 +0100

    Write a function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ar = malloc(sizeof(*arraynum) * ((max - min) + 1));
+	arraynum = malloc(sizeof(*arraynum) * ((max - min) + 1));
 
 	if (arraynum == NULL)
 		return (NULL);
