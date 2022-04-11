084fc4b0432870be24cfbc8ab2b7996a3095a6be
Jesubukade Ajakaye <ajakaye.bukade@gmail.com>
2022-04-11 23:47:00 +0100

    Write a function that frees dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,13 +11,13 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p_dog;
+	dog_t *n_dog;
 	int i, lname, lowner;
 
-	p_dog = malloc(sizeof(*p_dog));
-	if (p_dog == NULL || !(name) || !(owner))
+	n_dog = malloc(sizeof(*n_dog));
+	if (n_dog == NULL || !(name) || !(owner))
 	{
-		free(p_dog);
+		free(n_dog);
 		return (NULL);
 	}
 
@@ -27,26 +27,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (lowner = 0; owner[lowner]; lowner++)
 		;
 
-	p_dog->name = malloc(lname + 1);
-	p_dog->owner = malloc(lowner + 1);
+	n_dog->name = malloc(lname + 1);
+	n_dog->owner = malloc(lowner + 1);
 
-	if (!(p_dog->name) || !(p_dog->owner))
+	if (!(n_dog->name) || !(n_dog->owner))
 	{
-		free(p_dog->owner);
-		free(p_dog->name);
-		free(p_dog);
+		free(n_dog->owner);
+		free(n_dog->name);
+		free(n_dog);
 		return (NULL);
 	}
 
 	for (i = 0; i < lname; i++)
-		p_dog->name[i] = name[i];
-	p_dog->name[i] = '\0';
+		n_dog->name[i] = name[i];
+	n_dog->name[i] = '\0';
 
-	p_dog->age = age;
+	n_dog->age = age;
 
 	for (i = 0; i < lowner; i++)
-		p_dog->owner[i] = owner[i];
-	p_dog->owner[i] = '\0';
+		n_dog->owner[i] = owner[i];
+	n_dog->owner[i] = '\0';
 
-	return (p_dog);
+	return (n_dog);
 }
