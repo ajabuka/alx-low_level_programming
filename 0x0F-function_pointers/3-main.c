83a343e14b227645f057f534376eb3aaaf360945
Jesubukade Ajakaye <ajakaye.bukade@gmail.com>
2022-04-13 20:29:13 +0100

    Write a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code to perform simple arimethic operations.
  * @argc: argument count.
  * @argv: argument vector.
  * Return: Always 0.
