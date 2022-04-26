6a99cdb6ad93b2b848abb9ace1c2f4bf2d986513
Jesubukade Ajakaye <ajakaye.bukade@gmail.com>
2022-04-26 18:50:00 +0100

    Header file pushed

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,10 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * struct list_s - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
@@ -18,6 +17,19 @@ typedef struct listint_s
 	int n;;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 int _putchar(char);
 size_t print_listint(const listint_t *h);
